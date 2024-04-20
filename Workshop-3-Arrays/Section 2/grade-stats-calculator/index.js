d05d59efcb4a929892ddb63ad5215c8a73784416
AarFeE <andres.jimenezv@hotmail.com>
2024-04-19 21:28:34 -0500

    added few changes

--- Workshop-3-Arrays/Section 2/grade-stats-calculator/index.js
+++ Workshop-3-Arrays/Section 2/grade-stats-calculator/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+while (true) {
+    let grades = prompt("Insert your grades, separated by ',' and no spaces ' ':")
+    let flag = true
+
+    grades.forEach(grade => {
+        grade = Number(grade)
+    })
+
+
+
+    grades = grades.split(",")
+    let gradesAvg = grades.reduce((accum, value) => {
+        return accum + value
+    }, 0)
+
+    alert(gradesAvg)
+}
+
