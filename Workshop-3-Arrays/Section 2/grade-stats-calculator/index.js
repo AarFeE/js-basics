2526c3114030ab9a33ed440f316ce995c579d7ab
AarFeE <44449694+AarFeE@users.noreply.github.com>
2024-04-21 18:09:07 -0500

    Finished grade-stats-calculator and began with inventory

--- Workshop-3-Arrays/Section 2/grade-stats-calculator/index.js
+++ Workshop-3-Arrays/Section 2/grade-stats-calculator/index.js
@@ -1,18 +1,30 @@
-while (true) {
-    let grades = prompt("Insert your grades, separated by ',' and no spaces ' ':")
-    let flag = true
-
-    grades.forEach(grade => {
-        grade = Number(grade)
-    })
-
-
-
-    grades = grades.split(",")
-    let gradesAvg = grades.reduce((accum, value) => {
-        return accum + value
-    }, 0)
-
-    alert(gradesAvg)
+try {
+    main();
+} catch (error) {
+    alert(error.message);
 }
 
+function main() {
+    let grades = prompt('Insert the grades separated by a coma "," and avoid using spaces " ":')
+    grades = grades.split(',')
+
+    const castNumArray = (theArray) => {
+        let castArray = theArray.map((value) => {
+            if (isNaN(Number(value))) {
+                throw new Error("One of the inserted values wasn't a number")
+            } else {
+                return Number(value)
+            }
+        })
+        return castArray
+    }
+
+    grades = castNumArray(grades)
+
+    alert(`The average of the inserted grades is: ${grades.reduce((accum, value) => accum + value) / grades.length}\n` +
+        `The highest grade is: ${Math.max(...grades)}\n` +
+        `The lowest grade is: ${Math.min(...grades)}\n` +
+        `Number of approved grades: ${grades.filter((grade) => grade >= 70).length}\n` +
+        `Number of failed grades: ${grades.filter((grade) => grade < 70).length}\n` +
+        `Ordered grades from highest to lowest:\n${grades.sort((a, b) => b - a)}`)
+}
---
+++ Workshop-3-Arrays/Section 2/inventory-management-system/index.js
@@ -0,0 +1,32 @@
+const products = []
+let cont = 1
+
+while (true) {
+    let option = prompt("Insert the number of the option:\n\n" +
+        "1. Add New Product\n" +
+        "2. Duplicate Product\n" +
+        "3. Show Products / Look For One\n" +
+        "4. Update Product\n" +
+        "5. Delete Product\n" +
+        "6. Verify Product Supplies\n" +
+        "7. Sell Product\n" +
+        "8. Buy Products\n" +
+        "9. Inventory Total Value\n" +
+        "10. Products Sorting\n" +
+        "11. Products Description Filtering\n" +
+        "12. General Products Report\n" +
+        "13. Finish")
+
+    if (option == "13") {
+        alert("Thanks For Prefering Us!")
+        break
+    }
+
+    else if (option == "1") {
+
+    }
+
+    else {
+        alert("Not A Valid Option!")
+    }
+}
